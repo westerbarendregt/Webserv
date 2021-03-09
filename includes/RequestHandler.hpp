160d7b7d34bb0a0370fe84ee2a37ac6f6176a6d4
borispeeters <borispeeters.bp@gmail.com>
2021-03-09 14:43:19 +0100

    base request handling

--- includes/RequestHandler.hpp
+++ includes/RequestHandler.hpp
@@ -1,15 +1,45 @@
 #ifndef REQUEST_HANDLER
 # define REQUEST_HANDLER
 
+# include <map>
+# include <string>
 # include "Client.hpp"
 
 class RequestHandler
 {
 	public:
 		typedef	Client		t_client;
+		RequestHandler();
+		~RequestHandler();
 		void	handleMetadata(t_client &c);
 		void	handleRequest(t_client &c);
+
+		void printStatusCodes();
+		void printMimeTypes();
+
 	private:
+
+		std::string handleGET();
+		std::string handleHEAD();
+		std::string handlePOST();
+		std::string handlePUT();
+		std::string handleDELETE();
+
+		std::string Content_Type();
+
+		std::string statusLine();
+		std::string responseBody();
+		std::string responseHeaders();
+
+		std::string	generateErrorPage(int error);
+
+		std::map<int, std::string>	m_status_codes;
+		void	initStatusCodes();
+
+		std::map<std::string, std::string>	m_mime_types;
+		void	initMimeTypes();
+
+		std::vector<std::string>	m_response_headers;
 };
 
 #endif
--- srcs/RequestHandler.cpp
+++ srcs/RequestHandler.cpp
@@ -1,15 +1,98 @@
+#include <iostream>
 #include "RequestHandler.hpp"
 
+#define CRLF "\r\n"
+
+RequestHandler::RequestHandler() {
+	initStatusCodes();
+	initMimeTypes();
+}
+
+RequestHandler::~RequestHandler() {}
+
+std::string RequestHandler::Content_Type() {
+	std::string content_type;
+
+	return content_type;
+}
+
+std::string RequestHandler::statusLine() {
+	//
+}
+
+std::string RequestHandler::responseBody() {
+	//
+}
+
+std::string RequestHandler::responseHeaders() {
+
+	m_response_headers.push_back(Content_Type());
+}
+
+std::string RequestHandler::handleGET() {
+	std::string status_line = statusLine();
+	std::string response_body = responseBody();
+}
+
+std::string RequestHandler::handleHEAD() {
+	std::string status_line = statusLine();
+	std::string response_body = responseBody();
+}
+
+std::string RequestHandler::handlePOST() {
+	std::string status_line = statusLine();
+	std::string response_body = responseBody();
+}
+
+std::string RequestHandler::handlePUT() {
+	std::string status_line = statusLine();
+	std::string response_body = responseBody();
+}
+
+std::string RequestHandler::handleDELETE() {
+	std::string status_line = statusLine();
+	std::string response_body = responseBody();
+}
+
+std::string	RequestHandler::generateErrorPage(int error) {
+	std::string	error_response;
+
+	error_response =
+			"<html>" CRLF
+			"<head><title>" + itoa(error) + ' ' + m_status_codes[error] + "</title></head>" CRLF
+			"<body>" CRLF
+			"<center><h1>" + itoa(error) + ' ' + m_status_codes[error] + "</h1></center>" CRLF
+			;
+}
+
 void	RequestHandler::handleMetadata(t_client &c) {
-	std::cout<<"handling metadata.."<<std::endl;
 	c.m_request_data.m_done = true;
 }
 
 void	RequestHandler::handleRequest(t_client &c) {
- // either get resource or execute cgi, both populating the response and adding client_socket to write_all
- //
-	std::cout<<"handling request.."<<std::endl;
-	std::cout<<"generating response.."<<std::endl;
-	c.m_response_str = "HTTP/1.1 200 OK\r\nDate: Mon, 27 Jul 2009 12:28:53 GMT\r\nServer: webserv\r\nLast-Modified: Wed, 22 Jul 2009 19:15:56 GMT\r\nContent-Length: 4\r\nVary: Accept-Encoding\r\nContent-Type: text/plain\r\n\r\nOi\r\n";
- 
+	Request	&request = c.m_request_data;
+
+	if (request.m_error != 0) {
+		c.m_response_str = generateErrorPage(request.m_error);
+	} else if (/* cgi */) {
+		//
+	} else {
+		switch (request.m_method) {
+			case GET:
+				c.m_response_str = handleGET();
+				break;
+			case HEAD:
+				c.m_response_str = handleHEAD();
+				break;
+			case POST:
+				c.m_response_str = handlePOST();
+				break;
+			case PUT:
+				c.m_response_str = handlePUT();
+				break;
+			case DELETE:
+				c.m_response_str = handleDELETE();
+				break;
+		}
+	}
 }
