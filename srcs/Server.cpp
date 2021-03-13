d9a02f72f70d7feb72c345fb11779fb7b8906978
Tom olivier Tartarin <totartar@f0r3s24.codam.nl>
2021-03-13 17:23:52 +0100

    works on guac

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -5,6 +5,25 @@ Server::Server(char const *path) {
 	ConfigParser::parse(path, this->m_v_server_all);
 }
 
+	bool	ok(std::string const &lhs, std::string const &rhs) {
+		size_t	lhs_prefixes = 0;
+		size_t	rhs_prefixes = 0;
+		size_t	index = 0;
+
+		while ((index = lhs.find('/', index)) != std::string::npos) {
+			++index;
+			++lhs_prefixes;
+		}
+		index = 0;
+		while ((index = rhs.find('/', index)) != std::string::npos) {
+			++index;
+			++rhs_prefixes;
+		}
+		return (lhs_prefixes > rhs_prefixes
+				|| (lhs_prefixes == rhs_prefixes && lhs > rhs));
+	}
+
+
 std::vector<Server::t_v_server> *Server::getVirtualServer(int socket) {
 	for (t_v_server_all::iterator v_server = this->m_v_server_all.begin(); v_server != this->m_v_server_all.end(); ++v_server) {
 		if (v_server->second[0].m_socket == socket)
